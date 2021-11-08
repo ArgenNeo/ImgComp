93923973af882e8a4f6773aba735efa37ae83d00
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2021-11-09 00:07:58 +0700

    Improve threshold accuracy

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ difference = cv2.subtract(image1, image2)
 
 # color the mask red
 Conv_hsv_Gray = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
-ret, mask = cv2.threshold(Conv_hsv_Gray, 0, 255,cv2.THRESH_BINARY_INV |cv2.THRESH_OTSU)
+_, mask = cv2.threshold(Conv_hsv_Gray, 0, 255, cv2.THRESH_BINARY_INV)
 difference[mask != 255] = [0, 0, 255]
 
 # add the red mask to the images to make the differences obvious
